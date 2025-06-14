888d175067ab24725b6e3d410ef87655a5498e1b
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-14 13:31:35 +0500

    Added start.sh for Railway deployment

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Set fallback port
 PORT=$PORT
 if [ -z "$PORT" ]; then PORT=8000; fi
 
-# Train model
-python -c "from app.utils import train_and_save_model; train_and_save_model()"
-
-# Start FastAPI
+# Just start the server (no training)
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
