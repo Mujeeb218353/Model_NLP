b6f817ddd3da8ddc6e3e2bd6b2d67e47bdb7515f
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-22 01:44:54 +0500

    Trained Model on more data

--- app/utils.py
+++ app/utils.py
@@ -8,11 +8,13 @@ def train_and_save_model():
     df = pd.read_csv("Resume.csv")
     df.dropna(subset=["Resume_str", "Category"], inplace=True)
 
-    encoder = LabelEncoder()
+    encoder = LabelEncoder()    
     y = encoder.fit_transform(df["Category"])
 
     model = SentenceTransformer("all-MiniLM-L6-v2")
-    X = model.encode(df["Resume_str"].tolist(), show_progress_bar=True)
+
+    combined_texts = df["Resume_str"] + " " + df["Resume_html"]
+    X = model.encode(combined_texts.tolist(), show_progress_bar=True)
 
     clf = LogisticRegression(max_iter=1000)
     clf.fit(X, y)
